5d17754b76aacec3802c7210d91b8f1913b4cee7
FabriOrtiz40 <83090950+FabriOrtiz40@users.noreply.github.com>
2026-01-02 15:01:01 -0300

    Update main.go

--- main.go
+++ main.go
@@ -1,27 +1,43 @@
 package main
 
 import (
-    "encoding/csv"
-    "fmt"
-    "os"
+	"encoding/csv"
+	"fmt"
+	"os"
 )
 
 func main() {
-    file, err := os.Open("problems.csv")
-    if err != nil {
-        fmt.Println("Error al abrir el archivo:", err)
-        return
-    }
-    defer file.Close()
+	correctas := 0
 
-    reader := csv.NewReader(file)
-    records, err := reader.ReadAll()
-    if err != nil {
-        fmt.Println("Error al leer el CSV:", err)
-        return
-    }
+	file, err := os.Open("problems.csv")
+	if err != nil {
+		fmt.Println("Error al abrir el archivo:", err)
+		return
+	}
+	defer file.Close()
 
-    for _, record := range records {
-        fmt.Println("Pregunta:", record[0], "| Respuesta:", record[1])
-    }
+	reader := csv.NewReader(file)
+	records, err := reader.ReadAll()
+	if err != nil {
+		fmt.Println("Error al leer el CSV:", err)
+		return
+	}
+
+	for _, record := range records {
+		var respuesta string
+
+		fmt.Printf("¿%s? ", record[0])
+		fmt.Scanln(&respuesta)
+
+		if respuesta == record[1] {
+			fmt.Printf("Correctooou \n ")
+			correctas++
+		} else {
+			fmt.Printf("Maaal \n ")
+		}
+
+	}
+	total := len(records)
+	fmt.Printf("Respuestas correctas: %d de %d\n", correctas, total)
+
 }
